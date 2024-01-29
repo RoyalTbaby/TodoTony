bdb0ac3b87e90370a9a18d8afbaf64260811fa92
royal_T_baby <mashabodovskaya@gmail.com>
2024-01-29 18:43:20 +0100

    Bugs are fixed

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.http import JsonResponse
